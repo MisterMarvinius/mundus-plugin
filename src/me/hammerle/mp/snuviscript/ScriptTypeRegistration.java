0108a1eab686d8e16998cf8e1343c43b1095f299
Marvin <marvinius301@gmail.com>
2026-02-22 20:18:24 +0100

    sort type registration

--- src/me/hammerle/mp/snuviscript/ScriptTypeRegistration.java
+++ src/me/hammerle/mp/snuviscript/ScriptTypeRegistration.java
@@ -31,27 +31,27 @@ public final class ScriptTypeRegistration {
     private ScriptTypeRegistration() {}
 
     public static void registerMinecraftTypes() {
-        registerType("entity", Entity.class::isInstance);
-        registerType("living", LivingEntity.class::isInstance, "entity");
-        registerType("player", Player.class::isInstance, "living", "entity");
-        registerType("itemstack", ItemStack.class::isInstance);
-        registerType("itementity", Item.class::isInstance, "entity");
         registerType("block", Block.class::isInstance);
-        registerType("material", Material.class::isInstance);
-        registerType("location", Location.class::isInstance);
-        registerType("slot", EquipmentSlot.class::isInstance);
+        registerType("blockdisplay", BlockDisplay.class::isInstance, "display");
         registerType("damagesource", DamageSource.class::isInstance);
         registerType("display", Display.class::isInstance);
-        registerType("textdisplay", TextDisplay.class::isInstance, "display");
-        registerType("blockdisplay", BlockDisplay.class::isInstance, "display");
-        registerType("itemdisplay", ItemDisplay.class::isInstance, "display");
         registerType("enchantment", Enchantment.class::isInstance);
-        registerType("text", Component.class::isInstance);
-        registerType("table", Table.class::isInstance);
+        registerType("entity", Entity.class::isInstance);
         registerType("inventory", Inventory.class::isInstance);
-        registerType("world", World.class::isInstance);
+        registerType("itemdisplay", ItemDisplay.class::isInstance, "display");
+        registerType("itementity", Item.class::isInstance, "entity");
+        registerType("itemstack", ItemStack.class::isInstance);
+        registerType("living", LivingEntity.class::isInstance, "entity");
+        registerType("location", Location.class::isInstance);
+        registerType("material", Material.class::isInstance);
         registerType("particle", Particle.class::isInstance);
+        registerType("player", Player.class::isInstance, "living", "entity");
+        registerType("slot", EquipmentSlot.class::isInstance);
         registerType("sound", Sound.class::isInstance);
+        registerType("table", Table.class::isInstance);
+        registerType("text", Component.class::isInstance);
+        registerType("textdisplay", TextDisplay.class::isInstance, "display");
+        registerType("world", World.class::isInstance);
     }
 
     private static void registerType(String name, Predicate<Object> check, String... parents) {
